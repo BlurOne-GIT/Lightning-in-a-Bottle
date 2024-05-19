fe9677bc509b51064cb16d90841862c4a62bb2fb
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-05-19 01:12:34 -0300

    build: create project

---
+++ src/main/java/code/blurone/lightninginabottle/LightningBottle.kt
@@ -0,0 +1,13 @@
+package code.blurone.lightninginabottle
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class LightningBottle : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
