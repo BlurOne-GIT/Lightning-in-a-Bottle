a579d994f22ac0ed2f8ae0e1138e31cab6b07bc0
BlurOne! <61806094+BlurOne-GIT@users.noreply.github.com>
2024-05-21 23:25:21 -0300

    fix(Plugin): saveDefaultConfig

--- src/main/java/code/blurone/lightninginabottle/LightningBottle.kt
+++ src/main/java/code/blurone/lightninginabottle/LightningBottle.kt
@@ -4,6 +4,7 @@ import org.bukkit.plugin.java.JavaPlugin
 
 class LightningBottle : JavaPlugin() {
     override fun onEnable() {
+        saveDefaultConfig()
         config.getConfigurationSection("lightning-in-a-bottle")?.let {
             if (it.getBoolean("enabled", true))
                 server.pluginManager.registerEvents(LightningBrewer(it, this), this)
